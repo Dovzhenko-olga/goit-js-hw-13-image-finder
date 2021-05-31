cc274ecaf1824e8406b5961f21299ac014f616c3
Dovzhenko-olga <73553889+Dovzhenko-olga@users.noreply.github.com>
2021-05-31 18:55:04 +0300

    commit fetch work

---
+++ src/js/api-service.js
@@ -0,0 +1,37 @@
+const API_KEY = '21858854-4e72a628a4c4c72c060e1a9f9';
+
+const BASE_URL = `https://pixabay.com/api/?key=${API_KEY}`;
+const PER_PAGE = 12;
+
+export default class PicturesApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+  
+  fetchArticles() {
+    const options = {
+    Authorization: API_KEY,
+  };
+  
+  
+  return fetch(`${BASE_URL}&q=${this.searchQuery}&image_type=photo&per_page=${PER_PAGE}&page=${this.page}`, options)
+  .then(r => r.json())
+    .then(data => {
+      this.page += 1;
+      return data.hits;
+   });
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQery) {
+    this.searchQuery = newQery;
+  }
+}
---
+++ src/js/index.js
@@ -0,0 +1,93 @@
+import imageCardTpl from '/templates/imageCardTpl.hbs';
+import debounce from 'lodash.debounce';
+import PicturesApiService from '/js/api-service';
+import '/sass/main.scss';
+import { error } from '@pnotify/core';
+import '@pnotify/core/dist/BrightTheme.css';
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
+
+
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  imageContainer: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+};
+
+const picturesApiService = new PicturesApiService();
+
+refs.searchForm.addEventListener('input', debounce(onSearch, 500));
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onSearch(e) {
+
+  picturesApiService.query = e.target.value;
+
+  if (picturesApiService.query.length < 3) {
+    return error({
+      text: 'Please enter your specific query!',
+      mode: 'light',
+      closer: true,
+      hide: true,
+      sticker: false,
+      mouseReset: true,
+      shadow: true,
+      width: '350px',
+      minHeight: '14px',
+      delay: 2000,
+    });
+  }
+  picturesApiService.resetPage();
+  picturesApiService.fetchArticles()
+    .then(hits => {
+      if (!hits.length) {
+        error({
+          text: 'Please enter a more specific query!',
+          mode: 'dark',
+          closer: true,
+          hide: true,
+          sticker: false,
+          mouseReset: true,
+          shadow: true,
+          width: '350px',
+          minHeight: '14px',
+          delay: 2000,
+        });
+      }
+      clearGaleryContainer();
+      createGaleryMurcup(hits);
+      refs.loadMoreBtn.removeAttribute('disabled');
+    })
+    .catch(error => console.log(error));
+}
+
+function onLoadMore() {
+  picturesApiService.fetchArticles().then(createGaleryMurcup);
+}
+
+
+function createGaleryMurcup(hits) {
+  refs.imageContainer.insertAdjacentHTML('beforeend', imageCardTpl(hits));
+  
+  refs.imageContainer.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+  });
+}
+
+function clearGaleryContainer() {
+  refs.imageContainer.innerHTML = '';
+}
+
+
+refs.imageContainer.addEventListener('click', onImage);
+
+function onImage(e) {
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.url}"/>`
+  );
+  instance.show();
+}
